d325f41085b57186aff2cf9e0cc5d515ee114a65
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-06-13 13:38:25 -0300

    testando callback

---
+++ Intro component with sign-up form/js/script.js
@@ -0,0 +1,8 @@
+/* function digaNome (nome) {
+    alert("Olá! " + nome);
+} 
+function teste (callback) {
+    let nome = prompt("Por favor, insira seu nome.")
+    callback(nome);
+}
+teste(digaNome); */
