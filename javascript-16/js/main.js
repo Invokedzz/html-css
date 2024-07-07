0f85c33542da6130cf3f60ab9990d6b573925c36
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-07-06 21:39:49 -0300

    Update main.js

--- javascript-16/js/main.js
+++ javascript-16/js/main.js
@@ -1,25 +1,25 @@
 /* fetch('people.json').then(answer => answer.json()).then(json => loadElements(json)).catch(err => console.log(err)); */
 
-axios('people.json').then(answer => loadElements(answer.data));
+ //axios('people.json').then(answer => loadElements(answer.data)
 
 // Pode ser resolvido de uma das duas formas.
 
-function loadElements(json) {
+fetch('people.json').then(x => x.json()).then(json => loadElements(json)).catch(e => console.log(e));
+function loadElements (json) {
     const table = document.createElement('table');
-    for (let pessoa of json) {
+    for (let pessoas of json) {
         const tr = document.createElement('tr');
-        let td1 = document.createElement('td');
-        td1.innerHTML = pessoa.nome;
-        tr.appendChild(td1);
-        let td2 = document.createElement('td');
-        td2.innerHTML = pessoa.idade;
-        tr.appendChild(td2);
-        let td3 = document.createElement('td');
-        td3.innerHTML = pessoa.salario;
-        tr.appendChild(td3);
-
-        table.appendChild(tr)
+        let tda = document.createElement('td');
+        tda.innerHTML = pessoas.nome;
+        tr.appendChild(tda);
+        let tdb = document.createElement('td');
+        tdb.innerHTML = pessoas.estado;
+        tr.appendChild(tdb);
+        let tdc = document.createElement('td');
+        tdc.innerHTML = pessoas.sexo;
+        tr.appendChild(tdc)
+        table.appendChild(tr);
     }
     const resultado = document.querySelector('.resultado');
-    resultado.appendChild(table)
+    resultado.appendChild(table);
 }
