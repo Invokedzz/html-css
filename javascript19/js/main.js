5a598ab873a1138f242bbc4b0c8c29b7b3815231
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-07-17 08:46:14 -0300

    joguito

---
+++ javascript19/js/main.js
@@ -0,0 +1,16 @@
+let character = document.querySelector('.character');
+let terrain = document.querySelector('.block');
+function jumpBlock () {
+    if (character.classList !== 'animate') character.classList.add('animate');
+    setTimeout(function () {
+        character.classList.remove('animate');
+    }, 500)
+};
+let jumping = document.addEventListener('click', function () {
+    jumpBlock();
+});
+let checkIfdead = setInterval(function () {
+    let topChar = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+    let blockLeft = parseInt(window.getComputedStyle(terrain).getPropertyValue('left'));
+    if (blockLeft < 20 && blockLeft > 0 && topChar >= 130)alert('Morreu, seu horrível!');
+}, 10);
