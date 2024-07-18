376b414013bac1fabbe57f047cdea27e772f19ad
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-07-18 14:28:38 -0300

    cc

--- javascript19/js/main.js
+++ javascript19/js/main.js
@@ -1,17 +1,51 @@
 let character = document.querySelector('.character');
 let terrain = document.querySelector('.block');
-function jumpBlock () {
-    if (character.classList !== 'animate') character.classList.add('animate');
+let blueTerrain = document.querySelector('.block2');
+let isJumping = false;
+
+function jumpBlock() {
+    if (!isJumping) {
+        setTimeout(() => {
+            isJumping = true;
+            character.classList.add('animate');
+        }, 50);
+    }
     setTimeout(function () {
         character.classList.remove('animate');
-    }, 500)
+        isJumping = false;
+    }, 500);
 };
-let jumping = document.addEventListener('keydown', function (e) {
-        if (e.code === 'Space') jumpBlock();
-        if (e.code === 'ArrowUp') jumpBlock();
+
+document.addEventListener('keydown', function (e) {
+    if ((e.code === 'Space' || e.code === 'ArrowUp') && !isJumping) {
+        jumpBlock();
+    }
 });
-let checkIfdead = setInterval(function () {
+
+let checkIfdead1 = function () {
     let topChar = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
     let blockLeft = parseInt(window.getComputedStyle(terrain).getPropertyValue('left'));
-    if (blockLeft < 20 && blockLeft > 0 && topChar >= 130) alert('Você morreu');
-}, 10); 
+    return { topChar, blockLeft };
+};
+
+let checkIfdead2 = function () {
+    let blueLeft = parseInt(window.getComputedStyle(blueTerrain).getPropertyValue('bottom'));
+    return { blueLeft };
+};
+
+let checkCharacter = function () {
+    let topChar = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+    return { topChar };
+};
+
+ let checkDead = setInterval(function () {
+    let redBlock = checkIfdead1();
+    let blueBlock = checkIfdead2();
+    let characterHitbox = checkCharacter();
+
+    // Exemplo de verificação de colisão
+    if (redBlock.blockLeft < 60 && redBlock.blockLeft > 0 && characterHitbox.topChar >= 300) alert('Você morreu!');
+
+    if (blueBlock.blueLeft < 180 && blueBlock.blueLeft > 0 && characterHitbox.topChar >= 300) alert('You died!') 
+}, 10);
+
